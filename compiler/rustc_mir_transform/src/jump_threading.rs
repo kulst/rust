b9f50054f0dca60335c3bbc776e73a132d0a6a30
kulst <kulst@mailbox.org>
2025-03-18 22:35:25 +0100

    Disable JumpThreading for targets that have convergent operations

--- compiler/rustc_mir_transform/src/jump_threading.rs
+++ compiler/rustc_mir_transform/src/jump_threading.rs
@@ -62,7 +62,7 @@ const MAX_PLACES: usize = 100;
 
 impl<'tcx> crate::MirPass<'tcx> for JumpThreading {
     fn is_enabled(&self, sess: &rustc_session::Session) -> bool {
-        sess.mir_opt_level() >= 2
+        sess.mir_opt_level() >= 2 && !sess.target.has_convergent_ops
     }
 
     #[instrument(skip_all level = "debug")]
