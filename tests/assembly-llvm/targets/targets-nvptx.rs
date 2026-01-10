7aeab1d63a1140b14a41f538788bb885261e1c04
kulst <60887784+kulst@users.noreply.github.com>
2026-01-10 22:26:47 +0100

    Made NVPTX tests compatible with `-Ctarget-cpu` as target modifier
    - added `-Ctarget-cpu` flags
    - removed `core`-dependency where possible and added `minicore` where
      necessary
    - reenabled some tests

--- tests/assembly-llvm/targets/targets-nvptx.rs
+++ tests/assembly-llvm/targets/targets-nvptx.rs
@@ -2,7 +2,8 @@
 //@ assembly-output: emit-asm
 // ignore-tidy-linelength
 //@ revisions: nvptx64_nvidia_cuda
-//@ [nvptx64_nvidia_cuda] compile-flags: --target nvptx64-nvidia-cuda
+//@ [nvptx64_nvidia_cuda] compile-flags: --target nvptx64-nvidia-cuda -Ctarget-cpu=sm_30
+
 //@ [nvptx64_nvidia_cuda] needs-llvm-components: nvptx
 
 // Sanity-check that each target can produce assembly code.
