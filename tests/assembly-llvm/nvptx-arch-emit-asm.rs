57c21f4fb08946444b6415da0a400666236f6578
kulst <60887784+kulst@users.noreply.github.com>
2026-01-10 22:26:47 +0100

    Made NVPTX tests compatible with `-Ctarget-cpu` as target modifier
    - added `-Ctarget-cpu` flags
    - removed `core`-dependency where possible and added `minicore` where
      necessary
    - reenabled some tests

--- tests/assembly-llvm/nvptx-arch-emit-asm.rs
+++ tests/assembly-llvm/nvptx-arch-emit-asm.rs
@@ -1,8 +1,8 @@
 //@ assembly-output: emit-asm
-//@ compile-flags: --crate-type rlib
-//@ only-nvptx64
-
-#![no_std]
+//@ compile-flags: --target nvptx64-nvidia-cuda --crate-type rlib -Ctarget-cpu=sm_30
+//@ needs-llvm-components: nvptx
+#![feature(no_core)]
+#![no_core]
 
 // Verify default arch without ptx-linker involved.
 // CHECK: .target sm_30
